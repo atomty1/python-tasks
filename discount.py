2c76b73b0bd9fee66cecbba64b7efa5749b56f91
Taye Abidakun <tayeabidakun@gmail.com>
2025-01-31 20:27:07 +0100

    Welcome added to the discount page

--- discount.py
+++ discount.py
@@ -1,11 +1,12 @@
-# def discounts():
-#     amount = float(input("Enter the amount: "))
-#     discount_percent = float(input("Enter the percentage discount you want to give: "))
-#     discount_value = amount * discount_percent / 100
-#     user_pay = amount - discount_value
-#     print(f"You are to pay {user_pay} instead of {amount}, therefore you saved {discount_value}")
+def discounts():
+    print("Welcome")
+    amount = float(input("Enter the amount: "))
+    discount_percent = float(input("Enter the percentage discount you want to give: "))
+    discount_value = amount * discount_percent / 100
+    user_pay = amount - discount_value
+    print(f"You are to pay {user_pay} instead of {amount}, therefore you saved {discount_value}")
 
-# discounts()
+discounts()
 
 
 
